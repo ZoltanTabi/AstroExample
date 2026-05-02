b5043cfd811c1e11dc1bf1a671852cf3dc93089d
Zoltán Tábi <tabizoli98@gmail.com>
2026-05-02 16:17:18 +0200

    Added island (js framework) and them picker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astroexample-tabinho.netlify.app/"
+  site: "https://astroexample-tabinho.netlify.app/",
+  integrations: [preact()]
 });
